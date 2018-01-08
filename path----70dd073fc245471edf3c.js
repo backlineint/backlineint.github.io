1099894a2844cb1f812a8c09135305114935e2be
Brian Perry <contact.brianperry@gmail.com>
2018-01-07 22:15:53 -0600

    Added Gatsby build

---
+++ path----70dd073fc245471edf3c.js
@@ -0,0 +1,2 @@
+webpackJsonp([60335399758886],{148:function(t,a){t.exports={data:{site:{siteMetadata:{title:"Brian Perry"}}},layoutContext:{}}}});
+//# sourceMappingURL=path----70dd073fc245471edf3c.js.map
