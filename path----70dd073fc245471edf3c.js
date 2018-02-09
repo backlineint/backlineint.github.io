38834ec082e9c531d174378bc1a049dff4c1771e
Brian Perry <contact.brianperry@gmail.com>
2018-02-09 11:08:21 -0600

    2-9-18 build - MidCamp post

--- path----70dd073fc245471edf3c.js
+++ path----70dd073fc245471edf3c.js
@@ -1,2 +1,2 @@
-webpackJsonp([60335399758886],{117:function(t,a){t.exports={data:{site:{siteMetadata:{title:"Brian Perry"}}},layoutContext:{}}}});
+webpackJsonp([60335399758886],{118:function(t,a){t.exports={data:{site:{siteMetadata:{title:"Brian Perry"}}},layoutContext:{}}}});
 //# sourceMappingURL=path----70dd073fc245471edf3c.js.map
