e4430d39d39086ca768c6dbbe75317d9fa6f03c5
Brian Perry <contact.brianperry@gmail.com>
2018-04-17 17:46:47 -0500

    Revert "4-17-18 build - with images :)"
    This reverts commit 5309ced3cb4bbb503b295cf58b1a4d0b12930ab8.

--- path----70dd073fc245471edf3c.js
+++ path----70dd073fc245471edf3c.js
@@ -1,2 +1,2 @@
-webpackJsonp([60335399758886],{138:function(t,a){t.exports={data:{site:{siteMetadata:{title:"Brian Perry"}}},layoutContext:{}}}});
+webpackJsonp([60335399758886],{121:function(t,a){t.exports={data:{site:{siteMetadata:{title:"Brian Perry"}}},layoutContext:{}}}});
 //# sourceMappingURL=path----70dd073fc245471edf3c.js.map
